5efc0e073ae085741b1564b628a70695a40e8b35
Joao Gabriel <gah.mp1@gmail.com>
2026-04-10 23:07:53 -0300

    refactor: use constant body in stub.NotImplemented, drop json.Marshal

--- server/internal/stub/stub.go
+++ server/internal/stub/stub.go
@@ -1,15 +1,13 @@
 package stub
 
-import (
-	"encoding/json"
-	"net/http"
-)
+import "net/http"
+
+var notImplementedBody = []byte(`{"error":"not implemented"}`)
 
 // NotImplemented writes a 501 JSON response.
 // Use this in handler functions that have not yet been transcribed from the Kotlin source.
 func NotImplemented(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusNotImplemented)
-	b, _ := json.Marshal(map[string]string{"error": "not implemented"})
-	w.Write(b)
+	_, _ = w.Write(notImplementedBody)
 }
